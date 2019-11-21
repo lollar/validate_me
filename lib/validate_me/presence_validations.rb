d1d2f3be9f362659fd6a55ab8edfed1f5cebe3bb
lollar <mike@lollar.codes>
2019-11-21 16:39:07 -0700

    Updates all validation objects to have consistent interface

--- lib/validate_me/presence_validations.rb
+++ lib/validate_me/presence_validations.rb
@@ -5,7 +5,19 @@ module ValidateMe
     def self.call base_class:, column:
       return nil if column.null || column.null.nil?
 
+      new(column).validate base_class
+    end
+
+    def initialize column
+      @column = column
+    end
+
+    def validate base_class
       base_class.send :validates, column.name, presence: true
     end
+
+    private
+
+    attr_reader :column
   end
 end
--- lib/validate_me/varchar_limit_validations.rb
+++ lib/validate_me/varchar_limit_validations.rb
@@ -5,7 +5,19 @@ module ValidateMe
     def self.call base_class:, column:
       return nil unless column.type == :string && column.limit.present?
 
+      new(column).validate base_class
+    end
+
+    def initialize column
+      @column = column
+    end
+
+    def validate base_class
       base_class.send :validates, column.name, length: { maximum: column.limit }
     end
+
+    private
+
+    attr_reader :column
   end
 end
